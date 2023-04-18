3f4716af8d76fe1f2064ef4173310fdaf0a9c4a0
tingxin <friendship-119@163.com>
2023-04-18 06:00:33 +0000

    update

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/nwcd-samples/glacierestore.git'
 EMAIL = 'friendship-119@163.com'
 AUTHOR = 'barry.xu'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
